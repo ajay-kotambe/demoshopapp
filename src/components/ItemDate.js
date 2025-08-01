60909e60889a6afe138f7067f1b2893398f9bfef
ajay-kotambe <kotambeajay07@gmail.com>
2025-08-01 18:47:34 +0530

    modified Item & added ItemDate Component

---
+++ src/components/ItemDate.js
@@ -0,0 +1,13 @@
+import "./ItemDate.css";
+
+function ItemDate() {
+  return (
+    <div className="mainDiv">
+      <span className="date">20</span> &nbsp;
+      <span className="date">July</span>&nbsp;
+      <span className="date">2015</span>&nbsp;
+    </div>
+  );
+}
+
+export default ItemDate;
