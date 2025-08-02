45457776ca8a01e1a5dc15106a29ea5a311d829d
ajay-kotambe <kotambeajay07@gmail.com>
2025-08-02 16:16:38 +0530

    used hook named useState for render the new data on UI

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,16 +1,32 @@
 import "./ProductItem.css";
+import { useState } from "react";
 import ProductDate from "./ProductDate";
 import Card from "./Card";
 
-function ProductItem(props) {
+const ProductItem = (props) => {
+  console.log("Before useState");
+  const [newTitle, setNewTitle] = useState(props.title);
+
+  console.log("After useState");
+  const clickHandler = () => {
+    setNewTitle("Popcorn");
+
+    // console.log("Added to the cart..!");
+  };
   return (
     <Card className="product-item">
       <ProductDate date={props.date} />
       <div className="product-item_description">
-        <h2>{props.title}</h2>
+        <h2>{newTitle}</h2>
       </div>
+      <input
+        type="button"
+        className="product-item_btn"
+        value="Add to Cart"
+        onClick={clickHandler}
+      />
     </Card>
   );
-}
+};
 
 export default ProductItem;
